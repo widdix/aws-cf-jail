9ea51667cf1b05e635bc09bca0f0602574a2a91a
Michael Wittig <post@michaelwittig.info>
2015-10-12 19:23:21 +0200

    first try

---
+++ index.js
@@ -0,0 +1,21 @@
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+var cloudformation = new AWS.CloudFormation();
+
+function wrapper(fn) {
+  return function(event, context) {
+    console.log(fn, '(): ', event);
+    cloudformation[fn](event, function(err, data) {
+      if (err) {
+        context.fail(err);
+      } else {
+        context.succeed(data);
+      }
+    });
+  };
+}
+
+// export allowed wrapped API requests as lambda handlers
+exports.createStack = wrapper('createStack');
+exports.deleteStack = wrapper('deleteStack');
+exports.updateStack = wrapper('updateStack');
